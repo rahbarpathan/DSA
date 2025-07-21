68e1517133c27de49b5ca2266f956fb19f224bfd
Rahbar Islam <136718191+rahbarpathan@users.noreply.github.com>
2025-07-21 16:17:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (69.03%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void getpermutation(vector<int>& nums, int idx, vector<vector<int>>& ans) {
         if (idx == nums.size()) {
-            ans.push_back(nums);
+            ans.push_back({nums});
             return;
         }
 
